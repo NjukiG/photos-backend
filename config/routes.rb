4c789bd0dea9cdf4fafcd4a3c680c788f4cfa801
George <gnjuki19@gmail.com>
2024-02-06 11:33:54 +0300

    Added routes to be able to nest photos in albums

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   resources :photos
-  resources :albums
+  # resources :albums
   resources :publishers do
     resources :albums, only: [:show, :index]
+  end
+
+  resources :albums do
+    # nested resource for reviews
+    resources :photos, only: [:show, :index]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # resources :albums do
-  #   # nested resource for reviews
-  #   resources :photos, only: [:show, :index]
-  # end
+ 
 end
